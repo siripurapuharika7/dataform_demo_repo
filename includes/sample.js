0e44e8980817f373a6512668da8704c1176a3e7a
Harika <Harika.Siripurapu@Bilvantis.io>
2024-02-27 04:12:44 +0000

    yes

--- includes/sample.js
+++ includes/sample.js
@@ -5,7 +5,7 @@
 //   const query = `
 //     SELECT table_name
 //     FROM your_project_id.your_dataset.INFORMATION_SCHEMA.TABLES
-//     WHERE table_schema = 'your_schema'
+//     WHERE table_schema = 'harika_dataset'
 //   `;
 //   const result = await bigquery.query(query);
 //   result.forEach(row => {
@@ -13,5 +13,7 @@
 //   });
 //   return tableNames;
 // }
+
+
  
 // module.exports = getTableNames();
